28742e6883e4883e571cc371c2c8f12342f681cf
Diptendulkar <diptendu.tu@gmail.com>
2019-06-28 15:39:15 +0530

    add images and alignment

--- app/src/main/java/dip/com/onboardingexample/MainActivity.java
+++ app/src/main/java/dip/com/onboardingexample/MainActivity.java
@@ -3,13 +3,61 @@ package dip.com.onboardingexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private TextView textMessage;
+    private TextView textHelp;
+    private ImageView imgSettings;
+    private ImageView imgMenu;
+    private RelativeLayout rlOverlay;
+    private LinearLayout llSettings;
+    private LinearLayout llMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initControls();
+    }
+    private void initControls() {
+        textMessage = (TextView) findViewById(R.id.textMessage);
+        textHelp = (TextView) findViewById(R.id.textHelp);
+        textHelp.setOnClickListener(this);
+        imgSettings = (ImageView) findViewById(R.id.imgSettings);
+        imgSettings.setOnClickListener(this);
+        imgMenu = (ImageView) findViewById(R.id.imgMenu);
+        imgMenu.setOnClickListener(this);
+        rlOverlay = (RelativeLayout) findViewById(R.id.rlOverlay);
+        llSettings = (LinearLayout) findViewById(R.id.llSettings);
+        llMenu = (LinearLayout) findViewById(R.id.llMenu);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imgSettings:
+                Toast.makeText(this, "Settings icon clicked", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.imgMenu:
+                Toast.makeText(this, "Menu icon clicked", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.textHelp:
+                if (textHelp.getText().toString().equals("Next")) {
+                    llSettings.setVisibility(View.GONE);
+                    llMenu.setVisibility(View.VISIBLE);
+                    textHelp.setText("Got It");
+                } else {
+                    rlOverlay.setVisibility(View.GONE);
+                    textMessage.setVisibility(View.VISIBLE);
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
